4cae2541ecfc23bce28fb928b0b4b34bb9b381e1
Folkmann <theme1256@gmail.com>
2016-10-27 14:56:44 +0200

    Startede ormen

---
+++ worm.py
@@ -0,0 +1,17 @@
+# Imports
+import socket
+
+# Find info about self
+print socket.gethostbyname(socket.gethostname())
+print socket.getfqdn()
+
+# Find target
+
+
+# Wait for previous to respond, if previous is given
+
+
+# Transfer to target
+
+
+# Halt and catch fire
